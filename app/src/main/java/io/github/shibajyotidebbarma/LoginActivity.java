259dde4c87e027b74bf85d71169ea21505313af9
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-03-23 11:47:41 +0530

    mAuth Login functionality to MainActivity with existing user is added

--- app/src/main/java/io/github/shibajyotidebbarma/LoginActivity.java
+++ app/src/main/java/io/github/shibajyotidebbarma/LoginActivity.java
@@ -1,11 +1,20 @@
 package io.github.shibajyotidebbarma;
 
 import android.content.Intent;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginPasswordText;
     private Button loginButton;
     private Button loginRegisterButton;
+    private ProgressBar loginProgress;
 
     private FirebaseAuth mAuth;
 
@@ -34,6 +44,49 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = (Button) findViewById(R.id.loginPageLoginButton);
         loginRegisterButton =(Button) findViewById(R.id.loginPageRegisterButton);
 
+        loginProgress = (ProgressBar) findViewById(R.id.loginProgress);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                String loginEmail = loginEmailText.getText().toString();
+                String loginPassword = loginPasswordText.getText().toString();
+
+
+                if(!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPassword)){
+
+
+                    loginProgress.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+
+                                sendToMain();
+
+                            }
+
+                            else{
+
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this,"Error: "+errorMessage, Toast.LENGTH_LONG).show();
+
+
+                            }
+                            loginProgress.setVisibility(View.INVISIBLE);
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
 
 
     }
@@ -47,13 +100,18 @@ public class LoginActivity extends AppCompatActivity {
 
         if(currentUser!=null){
 
-            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(mainIntent);
-            finish();
+            sendToMain();
 
         }
 
 
 
+    }
+
+    private void sendToMain() {
+
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
